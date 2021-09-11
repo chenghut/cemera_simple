8126078d4323a3b0aa183f5f98bd7a83dac59a16
chengjz <chengjz@hkdt.org>
2021-09-11 23:06:20 +0800

    hello word

--- app/src/main/java/com/mrcheng/cemera_simple/MainActivity.java
+++ app/src/main/java/com/mrcheng/cemera_simple/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         CameraManager cameraManager  = (CameraManager) getSystemService(CAMERA_SERVICE);
 
+
     }
 }
